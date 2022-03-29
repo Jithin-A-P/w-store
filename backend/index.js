489b8d876f91ced6d593657fca055a08a9e0cb08
Jithin A P <jithinap009@gmail.com>
2022-03-29 08:35:59 +0530

    Initial commit

---
+++ backend/index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+
+const port = 5000
+const app = express()
+
+app.get('/', (req, res) => {
+    res.send("Hello world")
+})
+
+app.listen(port, () => {
+    console.log(`App running on ${port}`)
+})
